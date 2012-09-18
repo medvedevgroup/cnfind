5c5a55632f993636ab5cf77243941d80fa88aa65
Paul Medvedev <pmedvedev@mars.(none)>
2012-09-18 16:31:19 -0700

    Added licence header to all source files

--- doc_walker.cpp
+++ doc_walker.cpp
@@ -1,3 +1,21 @@
+/*Copyright 2012, Paul Medvedev
+
+This file is part of Cnfind
+
+Cnfind is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Cnfind is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with cnfind (see file gpl.txt). If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
